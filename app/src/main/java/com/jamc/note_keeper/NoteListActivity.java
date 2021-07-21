40a1441a4e60eaacb630f23c4a91df041b68ed12
Jeff Muñoz <jmunozcastro@protonmail.com>
2021-07-20 20:41:19 -0600

    New Activity added

---
+++ app/src/main/java/com/jamc/note_keeper/NoteListActivity.java
@@ -0,0 +1,55 @@
+package com.jamc.note_keeper;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import androidx.appcompat.widget.Toolbar;
+
+import java.util.List;
+
+
+public class NoteListActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_note_list);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+
+        initializeDisplayContent();
+    }
+
+    private void initializeDisplayContent() {
+        ListView listNotes = findViewById(R.id.list_node);
+
+        List<NoteInfo> notes = DataManager.getInstance().getNotes();
+
+        ArrayAdapter<NoteInfo> adapterNotes = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, notes);
+
+        listNotes.setAdapter(adapterNotes);
+
+        listNotes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(NoteListActivity.this, NoteActivity.class);
+
+                startActivity(intent);
+            }
+        });
+    }
+
+}
